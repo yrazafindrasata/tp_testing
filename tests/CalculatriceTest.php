e44ce3ba365f9a0c43463d90e5e9593efd763895
yrazafindrasata <yrazafindrasata@yacast.fr>
2020-04-08 17:10:03 +0200

    fix

--- tests/CalculatriceTest.php
+++ tests/CalculatriceTest.php
@@ -2,14 +2,15 @@
 namespace Tests;
 
 use App\Calculatrice;
+use PHPUnit\Framework\TestCase;
 
-class CalculatriceTest extends \PHPUnit\Framework\TestCase
+class CalculatriceTest extends TestCase
 {
     public $a = 8;
     public $b = 16;
 
     public function testAddition(){
         $foo = Calculatrice::addition($this->a, $this->b);
-        $this -> assertEquals($this->a+$this->b,$foo, "fail");
+        $this -> assertEquals($this->a+$this->b,$foo);
     }
 }
