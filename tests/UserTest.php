4c97dff526e12e5c79a1143800e8ae11f1cda489
yrazafindrasata <yrazafindrasata@yacast.fr>
2020-04-09 12:00:23 +0200

    ex3

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -2,25 +2,52 @@
 namespace Test;
 
 use App\User;
+use phpDocumentor\Reflection\Types\Void_;
 use PHPUnit\Framework\TestCase;
 
 class UserTest extends TestCase
 {
+    /** @var User $user */
+    private $user;
+
+
+    public function setUp(): void
+    {
+        $this -> user = new User('toto@email.com', 'toto', 'foo', 14);
+    }
+
+    public function tearDown(): void
+    {
+        unset($this->user);
+    }
+
     public function testIsValid()
     {
         $user = new User('toto@email.com', 'toto', 'foo', 14);
-        $this->assertTrue($user->isValid());
+        $this->assertTrue($this->user->isValid());
+    }
 
-        $user = new User('totomail', 'toto', 'foo', 15);
-        $this->assertFalse($user->isValid());
 
-        $user = new User('toto@email.com', '', 'foo', 15);
-        $this->assertFalse($user->isValid());
+    public function testBadEmail(){
+        $this->user->setEmail('totomail');
+        $this->assertFalse($this->user->isValid());
+        $this->user->setEmail('toto@email.com');
+    }
 
-        $user = new User('toto@email.com', 'toto', '', 15);
-        $this->assertFalse($user->isValid());
+    public function testBadAge(){
+        $this->user->setAge(12);
+        $this->assertFalse($this->user->isValid());
+        $this->user->setAge(14);
+    }
+    public function testBadFirstName(){
+        $this->user->setFirstName('');
+        $this->assertFalse($this->user->isValid());
+        $this->user->setFirstName('toto');
+    }
 
-        $user = new User('toto@email.com', 'toto', 'foo', 9);
-        $this->assertFalse($user->isValid());
+    public function testBadLastName(){
+        $this->user->setLastname('');
+        $this->assertFalse($this->user->isValid());
+        $this->user->setLastname('foo');
     }
 }
