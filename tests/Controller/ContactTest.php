9d7e6e9c93445b1d05ab90500d3657487c6ae815
yrazafindrasata <49917582+yrazafindrasata@users.noreply.github.com>
2020-04-13 18:50:48 +0200

    Update ContactTest.php

--- tests/Controller/ContactTest.php
+++ tests/Controller/ContactTest.php
@@ -46,4 +46,28 @@ class ContactTest extends WebTestCase
         ]);
         $this->assertSelectorTextContains('ul li','This value should be of type digit.');
     }
+    
+        public function  testEditNotOk()
+    {
+        $client = static::createClient();
+        $client->followRedirects(true);
+        $crawler = $client->request('GET','/contact/1/edit');
+        $this->assertResponseIsSuccessful();
+        $client->submitForm('Update',[
+            'contact[phone_number]' => '06564323er'
+        ]);
+        $this->assertSelectorTextContains('ul li','This value should be of type digit.');
+    }
+
+    public function  testEditOk()
+    {
+        $client = static::createClient();
+        $client->followRedirects(true);
+        $crawler = $client->request('GET','/contact/1/edit');
+        $this->assertResponseIsSuccessful();
+        $client->submitForm('Update',[
+            'contact[phone_number]' => '0656432367'
+        ]);
+        $this->assertSelectorTextContains('h1','Contact index');
+    }
 }
